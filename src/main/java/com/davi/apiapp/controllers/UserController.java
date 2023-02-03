4f941cfa85b09d139ee9261534f759d39c56296e
Davi <davimargelo@hotmail.com>
2023-02-03 18:40:39 -0300

    Using Model Mapper to parse User entity to DTO

--- src/main/java/com/davi/apiapp/controllers/UserController.java
+++ src/main/java/com/davi/apiapp/controllers/UserController.java
@@ -1,7 +1,8 @@
 package com.davi.apiapp.controllers;
 
-import com.davi.apiapp.domain.User;
+import com.davi.apiapp.dto.UserDTO;
 import com.davi.apiapp.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService userService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 }
