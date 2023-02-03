251392972132c58591b54a9482f6cc4bcbeeffa4
Davi <davimargelo@hotmail.com>
2023-02-03 18:57:59 -0300

    Endpoint create users

--- src/main/java/com/davi/apiapp/services/impl/UserServiceImpl.java
+++ src/main/java/com/davi/apiapp/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.davi.apiapp.services.impl;
 
 import com.davi.apiapp.domain.User;
+import com.davi.apiapp.dto.UserDTO;
 import com.davi.apiapp.repositories.UserRepository;
 import com.davi.apiapp.services.UserService;
 import com.davi.apiapp.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> userOptional = userRepository.findById(id);
@@ -25,5 +30,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO userDTO) {
+        return userRepository.save(mapper.map(userDTO, User.class));
     }
 }
