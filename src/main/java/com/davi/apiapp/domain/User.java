652fcd1e9921190fa7f3ef2d5a7a851118a4f5bd
davimargelo <davi@DESKTOP-144TG4M>
2023-02-03 17:49:35 -0300

    added domain package

---
+++ src/main/java/com/davi/apiapp/domain/User.java
@@ -0,0 +1,26 @@
+package com.davi.apiapp.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+@Entity(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include()
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
