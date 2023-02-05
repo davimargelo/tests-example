e2bb1b68b7547175efc67b3a76f729cb3b02e6dc
Davi <davimargelo@hotmail.com>
2023-02-05 13:43:21 -0300

    User delete test with success

--- src/test/java/com/davi/apiapp/services/impl/UserServiceImplTest.java
+++ src/test/java/com/davi/apiapp/services/impl/UserServiceImplTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -161,7 +161,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser() {
